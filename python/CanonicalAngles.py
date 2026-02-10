8edb6f0c158b116aff61a19e21c3aa3b750c1b08
apu727 <bc528@charon>
2026-02-10 13:22:58 +0000

    Fixed python build, Improved canonical angles

--- python/CanonicalAngles.py
+++ python/CanonicalAngles.py
@@ -1,5 +1,6 @@
 from PyAnsatzEvolve import stateAnsatzManager, loadPath, loadParameters,readCsvState,loadOperators,loadNuclearEnergy
 import numpy as np
+import os
 
 
 def CanonicaliseAngles(man,Angles,operators, SDSPositions, InitialStateCoeffs, InitialStateIndices, invertSignOfWavefunction = False):
@@ -20,119 +21,138 @@ def CanonicaliseAngles(man,Angles,operators, SDSPositions, InitialStateCoeffs, I
     #print(f"Angles before {Angles}")
 
     InitialStateBitstring = InitialStateIndices[0]
-    for i,L in enumerate(SDSPositions):
-        #We implement v = EXC sin(-theta)  + (1-cos(-theta)) Exc^2 as the rotation. See Manual.md
-        #To fix this we invert the signs here. 
+    for loop in range(2):
+        for i,L in enumerate(SDSPositions):
+            #We implement v = EXC sin(-theta)  + (1-cos(-theta)) Exc^2 as the rotation. See Manual.md
+            #To fix this we invert the signs here. 
 
-        Angle1 = -Angles[L[0][0]] #S
-        Angle2 = -Angles[L[1][0]] #D
-        Angle3 = -Angles[L[2][0]] #S
+            Angle1 = -Angles[L[0][0]] #S
+            Angle2 = -Angles[L[1][0]] #D
+            Angle3 = -Angles[L[2][0]] #S
 
-        #scoping
-        def check():
-            #Check the assumptions
-            doubleExc = operators[L[1][0]]
-            S1 = operators[L[0][0]]
-            S2 = operators[L[0][1]]
-            S3 = operators[L[2][0]]
-            S4 = operators[L[2][1]]
-            #Check that the single double single structure has the spin operators together and not commuted or anything
-            assert(S1 == S3 and S2 == S4)
-            #Check that they are actually spin operators
-            assert(S1 != S2 and S3 != S4)
-            #Check that the double acts on the correct bits
-            assert(set(doubleExc).union([0]) == set(S1).union(S2))
-            #check that the double has the correct ordering and therefore phase
-            assert(doubleExc[0] == S1[0] and doubleExc[1] == S2[0] and doubleExc[2] == S1[1] and doubleExc[3] == S2[1])
-        check()
-        
-        
-                                                
-        #Use the single excitations to determine the space we are acting on
-        SpinDownAOs = [operators[L[0][0]][0]-1,operators[L[0][0]][1]-1]
-        SpinUpAOs = [operators[L[0][1]][0]-1,operators[L[0][1]][1]-1]
-        startVector = np.array([[0],[0],[0]],dtype=np.float64) # Isomorphism is X axis is 01|01, Y axis is 0.707 01|10 + 0.707 10|01, Z axis is 10|10
-        InitialStateBitstringUnset = InitialStateBitstring ^ (InitialStateBitstring & ((1<<SpinDownAOs[0]) | (1<<SpinDownAOs[1]) | (1<<SpinUpAOs[0]) | (1<<SpinUpAOs[0])))
-        
-        #We are assuming this is a single configuration initial state and therefore we are never along the Y axis. 
-        #SpinDownAOs[1] contains the annihilate position
-        
-        if InitialStateBitstring == (InitialStateBitstringUnset | (1<<SpinDownAOs[1]) | (1<<SpinUpAOs[1])): #X state 
-            startVector[0,0] = 1.
-        elif InitialStateBitstring == (InitialStateBitstringUnset | (1<<SpinDownAOs[0]) | (1<<SpinUpAOs[0])): #Z state
-            startVector[2,0] = 1.
-        elif InitialStateBitstring == (InitialStateBitstringUnset | (1<<SpinDownAOs[0]) | (1<<SpinUpAOs[1])) or \
-            InitialStateBitstring == (InitialStateBitstringUnset | (1<<SpinDownAOs[1]) | (1<<SpinUpAOs[0])) : #Part of Y state
-            assert(False) #Y state is not handled. 
-        elif InitialStateBitstring == InitialStateBitstringUnset or \
-            InitialStateBitstring == (InitialStateBitstring | ((1<<SpinDownAOs[0]) | (1<<SpinDownAOs[1]) | (1<<SpinUpAOs[0]) | (1<<SpinUpAOs[0]))):
-            # if the bitstring is 0000 or 1111 then all the angles do nothing so set them to zero. 
-            Angles[L[0][0]] = 0 #S Down
-            Angles[L[0][1]] = 0 #S up
-            Angles[L[1][0]] = 0 #D
-            Angles[L[2][0]] = 0 #S Down
-            Angles[L[2][1]] = 0 #S up
-            continue
-        else:
-            #1 or 3 are set. Only the singles have an effect. Condense them down. 
-            Angles[L[0][0]] += Angles[L[2][0]] #S Down
-            Angles[L[0][1]] += Angles[L[2][1]] #S up
-            Angles[L[1][0]] = 0 #D
-            Angles[L[2][0]] = 0 #S Down
-            Angles[L[2][1]] = 0 #S up
-            continue
-        
-        #find the resultant vector:
-        #Setup rotation generators
-        Kappa1 = np.array([[0,-np.sqrt(2),0],[np.sqrt(2),0,-np.sqrt(2)],[0,np.sqrt(2),0]]) # single exc
-        Kappa2 = np.array([[0,0,-2],[0,0,0],[2,0,0]],dtype=np.float64) # double exc
-        resultVector = np.array(startVector.copy())
-        if invertSignOfWavefunction and not managedToInvertWaveFuctionSign:
-            resultVector *= -1
+            #scoping
+            def check():
+                #Check the assumptions
+                doubleExc = operators[L[1][0]]
+                S1 = operators[L[0][0]]
+                S2 = operators[L[0][1]]
+                S3 = operators[L[2][0]]
+                S4 = operators[L[2][1]]
+                #Check that the single double single structure has the spin operators together and not commuted or anything
+                assert(S1 == S3 and S2 == S4)
+                #Check that they are actually spin operators
+                assert(S1 != S2 and S3 != S4)
+                #Check that the double acts on the correct bits
+                assert(set(doubleExc).union([0]) == set(S1).union(S2))
+                #check that the double has the correct ordering and therefore phase
+                assert(doubleExc[0] == S1[0] and doubleExc[1] == S2[0] and doubleExc[2] == S1[1] and doubleExc[3] == S2[1])
+            check()
+            
+            
+                                                    
+            #Use the single excitations to determine the space we are acting on
+            SpinDownAOs = [operators[L[0][0]][0]-1,operators[L[0][0]][1]-1]
+            SpinUpAOs = [operators[L[0][1]][0]-1,operators[L[0][1]][1]-1]
+            startVector = np.array([[0],[0],[0]],dtype=np.float64) # Isomorphism is X axis is 01|01, Y axis is 0.707 01|10 + 0.707 10|01, Z axis is 10|10
+            InitialStateBitstringUnset = InitialStateBitstring ^ (InitialStateBitstring & ((1<<SpinDownAOs[0]) | (1<<SpinDownAOs[1]) | (1<<SpinUpAOs[0]) | (1<<SpinUpAOs[1])))
+            
+            #We are assuming this is a single configuration initial state and therefore we are never along the Y axis. 
+            #SpinDownAOs[1] contains the annihilate position
+            
+            if InitialStateBitstring == (InitialStateBitstringUnset | (1<<SpinDownAOs[1]) | (1<<SpinUpAOs[1])): #X state 
+                startVector[0,0] = 1.
+            elif InitialStateBitstring == (InitialStateBitstringUnset | (1<<SpinDownAOs[0]) | (1<<SpinUpAOs[0])): #Z state
+                startVector[2,0] = 1.
+            elif InitialStateBitstring == (InitialStateBitstringUnset | (1<<SpinDownAOs[0]) | (1<<SpinUpAOs[1])) or \
+                InitialStateBitstring == (InitialStateBitstringUnset | (1<<SpinDownAOs[1]) | (1<<SpinUpAOs[0])) : #Part of Y state
+                assert(False) #Y state is not handled. 
+            elif InitialStateBitstring == InitialStateBitstringUnset or \
+                InitialStateBitstring == (InitialStateBitstring | ((1<<SpinDownAOs[0]) | (1<<SpinDownAOs[1]) | (1<<SpinUpAOs[0]) | (1<<SpinUpAOs[1]))):
+                # if the bitstring is 0000 or 1111 then all the angles do nothing so set them to zero. 
+                Angles[L[0][0]] = 0 #S Down
+                Angles[L[0][1]] = 0 #S up
+                Angles[L[1][0]] = 0 #D
+                Angles[L[2][0]] = 0 #S Down
+                Angles[L[2][1]] = 0 #S up
+                continue
+            else:
+                #1 or 3 are set. Only the singles have an effect. Condense them down. 
+                Angles[L[0][0]] += Angles[L[2][0]] #S Down
+                # Angles[L[0][1]] += Angles[L[2][1]] #S up
+                Angles[L[1][0]] = 0 #D
+                Angles[L[2][0]] = 0 #S Down
+                Angles[L[2][1]] = 0 #S up
+                
+                Angles[L[0][0]] = np.mod(Angles[L[0][0]],2*np.pi)
+                #Keep the angle in the upper half plane, the negative half plane is related by a negative sign. this is [0,Pi]
+                if loop == 0:
+                    if Angles[L[0][0]] > np.pi:
+                        managedToInvertWaveFuctionSign = not managedToInvertWaveFuctionSign
+                        Angles[L[0][0]] -= np.pi
+                elif managedToInvertWaveFuctionSign != invertSignOfWavefunction:
+                    managedToInvertWaveFuctionSign = not managedToInvertWaveFuctionSign
+                    Angles[L[0][0]] += np.pi
+                
+                Angles[L[0][1]]  = Angles[L[0][0]] 
+                continue
+            
+            #find the resultant vector:
+            #Setup rotation generators
+            Kappa1 = np.array([[0,-np.sqrt(2),0],[np.sqrt(2),0,-np.sqrt(2)],[0,np.sqrt(2),0]]) # single exc
+            Kappa2 = np.array([[0,0,-2],[0,0,0],[2,0,0]],dtype=np.float64) # double exc
+            resultVector = np.array(startVector.copy())
+            
 
-        resultVector += 0.5*np.sin(2*Angle1)*Kappa1@resultVector + 0.25*(1-np.cos(2*Angle1)) * Kappa1 @ Kappa1 @ resultVector
-        resultVector += 0.5*np.sin(Angle2)*Kappa2@resultVector + 0.25*(1-np.cos(Angle2)) * Kappa2 @ Kappa2 @ resultVector
-        resultVector += 0.5*np.sin(2*Angle3)*Kappa1@resultVector + 0.25*(1-np.cos(2*Angle3)) * Kappa1 @ Kappa1 @ resultVector
-        
+            resultVector += 0.5*np.sin(2*Angle1)*Kappa1@resultVector + 0.25*(1-np.cos(2*Angle1)) * Kappa1 @ Kappa1 @ resultVector
+            resultVector += 0.5*np.sin(Angle2)*Kappa2@resultVector + 0.25*(1-np.cos(Angle2)) * Kappa2 @ Kappa2 @ resultVector
+            resultVector += 0.5*np.sin(2*Angle3)*Kappa1@resultVector + 0.25*(1-np.cos(2*Angle3)) * Kappa1 @ Kappa1 @ resultVector
+            
+            if loop == 0:
+                if resultVector[0] + resultVector[1] + resultVector[2] < 0:
+                    resultVector *= -1
+                    managedToInvertWaveFuctionSign = not managedToInvertWaveFuctionSign
+            elif managedToInvertWaveFuctionSign != invertSignOfWavefunction:
+                resultVector *= -1
+                managedToInvertWaveFuctionSign = not managedToInvertWaveFuctionSign
+            
 
-        #We now need to get back to the starting vector while enforcing the SDS^{-1} structure
-        
-        #Magic algebra
-        # The SDS^{-1} structure can be thought of as doing the kappa2 rotation in a modified X' Z' plane. 
-        # Find the angle of the S required such that Y'.startVector = Y'.endVector. There are two such angles at pi apart. Pick the smaller one
+            #We now need to get back to the starting vector while enforcing the SDS^{-1} structure
+            
+            #Magic algebra
+            # The SDS^{-1} structure can be thought of as doing the kappa2 rotation in a modified X' Z' plane. 
+            # Find the angle of the S required such that Y'.startVector = Y'.endVector. There are two such angles at pi apart. Pick the smaller one
 
-        # theta1 = -np.atan2((resultVector[1,0]-startVector[1,0])*np.sqrt(2),startVector[2,0]-startVector[0,0]+resultVector[0,0]-resultVector[2,0])/2
-        theta1 = -np.atan(((resultVector[1,0]-startVector[1,0])*np.sqrt(2))/(startVector[2,0]-startVector[0,0]+resultVector[0,0]-resultVector[2,0]))/2
-        XPrime = 0.5*np.sin(-2*theta1)*Kappa1@[[1],[0],[0]] + 0.25*(1-np.cos(-2*theta1)) * Kappa1 @ Kappa1 @ [[1],[0],[0]] + [[1],[0],[0]]
-        YPrime = 0.5*np.sin(-2*theta1)*Kappa1@[[0],[1],[0]] + 0.25*(1-np.cos(-2*theta1)) * Kappa1 @ Kappa1 @ [[0],[1],[0]] + [[0],[1],[0]]
-        ZPrime = 0.5*np.sin(-2*theta1)*Kappa1@[[0],[0],[1]] + 0.25*(1-np.cos(-2*theta1)) * Kappa1 @ Kappa1 @ [[0],[0],[1]] + [[0],[0],[1]]
-        SX,SZ = np.dot(startVector[:,0],XPrime[:,0]),np.dot(startVector[:,0],ZPrime[:,0])
-        VX,VZ = np.dot(resultVector[:,0],XPrime[:,0]),np.dot(resultVector[:,0],ZPrime[:,0])
-        
-        assert(np.isclose(np.dot(startVector[:,0],YPrime[:,0]), np.dot(resultVector[:,0],YPrime[:,0]))) # ,"Y'.V == Y'.S is false" 
+            # theta1 = -np.atan2((resultVector[1,0]-startVector[1,0])*np.sqrt(2),startVector[2,0]-startVector[0,0]+resultVector[0,0]-resultVector[2,0])/2
+            theta1 = -np.atan(((resultVector[1,0]-startVector[1,0])*np.sqrt(2))/(startVector[2,0]-startVector[0,0]+resultVector[0,0]-resultVector[2,0]))/2
+            XPrime = 0.5*np.sin(-2*theta1)*Kappa1@[[1],[0],[0]] + 0.25*(1-np.cos(-2*theta1)) * Kappa1 @ Kappa1 @ [[1],[0],[0]] + [[1],[0],[0]]
+            YPrime = 0.5*np.sin(-2*theta1)*Kappa1@[[0],[1],[0]] + 0.25*(1-np.cos(-2*theta1)) * Kappa1 @ Kappa1 @ [[0],[1],[0]] + [[0],[1],[0]]
+            ZPrime = 0.5*np.sin(-2*theta1)*Kappa1@[[0],[0],[1]] + 0.25*(1-np.cos(-2*theta1)) * Kappa1 @ Kappa1 @ [[0],[0],[1]] + [[0],[0],[1]]
+            SX,SZ = np.dot(startVector[:,0],XPrime[:,0]),np.dot(startVector[:,0],ZPrime[:,0])
+            VX,VZ = np.dot(resultVector[:,0],XPrime[:,0]),np.dot(resultVector[:,0],ZPrime[:,0])
+            
+            assert(np.isclose(np.dot(startVector[:,0],YPrime[:,0]), np.dot(resultVector[:,0],YPrime[:,0]))) # ,"Y'.V == Y'.S is false" 
 
-        XZPrimeAngleStart = np.atan2(SZ,SX)
-        XZPrimeAngleEnd = np.atan2(VZ,VX)
-        theta2 = XZPrimeAngleEnd - XZPrimeAngleStart
-        if theta2 > np.pi:
-            theta2 = 2*np.pi - theta2
-        elif theta2 < -np.pi:
-            theta2 = theta2 + 2*np.pi 
+            XZPrimeAngleStart = np.atan2(SZ,SX)
+            XZPrimeAngleEnd = np.atan2(VZ,VX)
+            theta2 = XZPrimeAngleEnd - XZPrimeAngleStart
+            if theta2 > np.pi:
+                theta2 = 2*np.pi - theta2
+            elif theta2 < -np.pi:
+                theta2 = theta2 + 2*np.pi 
 
-        #Check that theta1 and theta2 take us to the correct vector
-        resultVector2 = np.array(startVector.copy())
-        resultVector2 += 0.5*np.sin(2*theta1)*Kappa1@resultVector2 + 0.25*(1-np.cos(2*theta1)) * Kappa1 @ Kappa1 @ resultVector2
-        resultVector2 += 0.5*np.sin(theta2)*Kappa2@resultVector2 + 0.25*(1-np.cos(theta2)) * Kappa2 @ Kappa2 @ resultVector2
-        resultVector2 += 0.5*np.sin(-2*theta1)*Kappa1@resultVector2 + 0.25*(1-np.cos(-2*theta1)) * Kappa1 @ Kappa1 @ resultVector2
+            #Check that theta1 and theta2 take us to the correct vector
+            resultVector2 = np.array(startVector.copy())
+            resultVector2 += 0.5*np.sin(2*theta1)*Kappa1@resultVector2 + 0.25*(1-np.cos(2*theta1)) * Kappa1 @ Kappa1 @ resultVector2
+            resultVector2 += 0.5*np.sin(theta2)*Kappa2@resultVector2 + 0.25*(1-np.cos(theta2)) * Kappa2 @ Kappa2 @ resultVector2
+            resultVector2 += 0.5*np.sin(-2*theta1)*Kappa1@resultVector2 + 0.25*(1-np.cos(-2*theta1)) * Kappa1 @ Kappa1 @ resultVector2
 
-        # print(f"err{resultVector2 - resultVector}")
-        #We implement -EXC as the rotation
-        Angles[L[0][0]] = -theta1 #S Down
-        Angles[L[0][1]] = -theta1 #S up
-        Angles[L[1][0]] = -theta2 #D
-        Angles[L[2][0]] = theta1 #S Down
-        Angles[L[2][1]] = theta1 #S up
-        managedToInvertWaveFuctionSign = True
+            # print(f"err{resultVector2 - resultVector}")
+            #We implement -EXC as the rotation
+            Angles[L[0][0]] = -theta1 #S Down
+            Angles[L[0][1]] = -theta1 #S up
+            Angles[L[1][0]] = -theta2 #D
+            Angles[L[2][0]] = theta1 #S Down
+            Angles[L[2][1]] = theta1 #S up
                 
 
 
@@ -146,12 +166,17 @@ def CanonicaliseAngles(man,Angles,operators, SDSPositions, InitialStateCoeffs, I
         stateAfter = man.getFinalState()
         assert(np.isclose(np.dot(stateBefore,stateAfter),-1))
     else:
+        assert(not managedToInvertWaveFuctionSign)
         stateAfter = man.getFinalState()
         assert(np.isclose(np.dot(stateBefore,stateAfter),1))
     return Angles
 
 if __name__ == "__main__":
-    PathBase = "/users/bence/AnsatzEvolve/Hams/H4/L1/H4"
+    DoH4 = False
+    if DoH4:
+        PathBase = os.path.dirname(os.path.realpath(__file__)) + "/../Hams/H4/L1/H4"
+    else:
+        PathBase = os.path.dirname(os.path.realpath(__file__)) + "/../Hams/H6/L1/H6"
     
     #Load a template path to know how long it will be.
     templatePath = loadPath(PathBase + "_Operators.dat") # AKA qc_ucc.dat
@@ -178,7 +203,10 @@ if __name__ == "__main__":
             InitialStateCoeffs.append(v)
             InitialStateIndices.append(i)
     assert(len(InitialStateCoeffs) == 1)
-    # InitialStateIndices[0] = 0b00110011
+    if DoH4:
+        InitialStateIndices[0] = 0b01010011
+    else:
+        InitialStateIndices[0] = 0b001011011001 # 00 10 11 | 01 10 01 <- Has all three cases. 
     numberOfQubits = round(np.log2(len(InitialState)))
 
     #Load the ansatz,
@@ -198,7 +226,11 @@ if __name__ == "__main__":
     man.setAngles(Angles[1])
 
     #Hardcoded the SDS positions for the ansatz we are using, TODO determine this automatically its not trivial. 
-    SDSPositions = [[[0,1],[2],[3,4]],[[5,6],[7],[8,9]]]
+    if DoH4:
+        SDSPositions = [[[0,1],[2],[3,4]],[[5,6],[7],[8,9]]]
+    else:
+        SDSPositions = [[[0,1],[2],[3,4]],[[5,6],[7],[8,9]],[[10,11],[12],[13,14]]]
+
     #Compute the canonical angles for this wavefunction and check
     CanAngles = CanonicaliseAngles(man,Angles[2],Operators,SDSPositions,InitialStateCoeffs,InitialStateIndices)
     print(f"Angles before:\n {np.array(Angles[2])}\n went to:\n {CanAngles}")
@@ -213,16 +245,20 @@ if __name__ == "__main__":
         for i in range(0,10000):
             a = np.random.rand(numAngles)
             #Enforce the orderfile
-            #This has been tacked on not the true order file as this has the SDS structure already
-            order = [(0,1),(0,1),(1,1),(2,1),(2,1),(3,1),(3,1),(4,1),(5,1),(5,1),(6,1),(6,1),(7,1),(8,1),(8,1)]
+            #This has been tacked on not the true order file as that has the SDS^{-1} structure already
+            if DoH4:
+                order = [(0,1),(0,1),(1,1),(2,1),(2,1),(3,1),(3,1),(4,1),(5,1),(5,1),(6,1),(6,1),(7,1),(8,1),(8,1)]
+                compA = np.zeros(9) # numUniqueParameters
+            else:
+                order = [(0,1),(0,1),(1,1),(2,1),(2,1),(3,1),(3,1),(4,1),(5,1),(5,1),(6,1),(6,1),(7,1),(8,1),(8,1),(9,1),(9,1),(10,1),(11,1),(11,1),(12,1),(12,1),(13,1),(14,1),(14,1)]
+                compA = np.zeros(15) # numUniqueParameters
 
-            compA = np.zeros(9) # numUniqueParameters
             for j,o in enumerate(order):
                 compA[o[0]] = a[j]
             for j in range(len(a)):
                 a[j] = compA[order[j][0]]*order[j][1]
 
             CanAngles = CanonicaliseAngles(man,a,Operators,SDSPositions,InitialStateCoeffs,InitialStateIndices)
-            CanNegAngles = CanonicaliseAngles(man,Angles[2],Operators,SDSPositions,InitialStateCoeffs,InitialStateIndices,True)
+            CanNegAngles = CanonicaliseAngles(man,a,Operators,SDSPositions,InitialStateCoeffs,InitialStateIndices,True)
     fuzzing()
 
